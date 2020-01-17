329c992d66619a26f50e84a2786113af6ad2f2fc
AndreeaPai <and_r33a_94@yahoo.com>
2020-01-17 21:19:51 +0200

    Add files via upload
    Migrations

---
+++ 20191228114546_SeedDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArtShop.Migrations
+{
+    public partial class SeedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "OrderDate", "OrderNumber" },
+                values: new object[] { 1, new DateTime(2019, 12, 28, 11, 45, 46, 642, DateTimeKind.Utc).AddTicks(212), "12345" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
