687ac33fd594b4a2354a751d6cafce03e448e822
AndreeaPai <and_r33a_94@yahoo.com>
2020-01-17 21:22:11 +0200

    Add files via upload
    Services

---
+++ IMailService.cs
@@ -0,0 +1,8 @@
+﻿namespace ArtShop.Services
+{
+    //interfetele sunt folositoare daca vrei implementari diferite pt cazuri diferite
+    public interface IMailService
+    {
+        void SendMessage(string to, string subject, string body);
+    }
+}
---
+++ NullMailService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//service for testing
+
+namespace ArtShop.Services
+{
+    public class NullMailService : IMailService
+    {
+        //inject logger into our class, used to log information no matter where is going
+        //create a constructor ctor + tab
+        //cream un un read only field pt Ilogger
+        private readonly ILogger<NullMailService> _logger;
+        public NullMailService(ILogger<NullMailService> logger)
+        {
+            //legam in constr acel field logger creat mai sus
+            _logger = logger;
+        }
+
+        public void SendMessage(string to, string subject, string body)
+        {
+            //log the message,but not actually send it
+            _logger.LogInformation($"To: {to} Subject: {subject} Body: {body}");
+        }
+    }
+}
